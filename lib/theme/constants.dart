ccc8dbb0a4bee335cacb561d9961f4430ef71a4c
umairjamil11 <umairjamil348@gmail.com>
2021-06-21 21:49:22 -0700

    first commit

---
+++ lib/theme/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xff);
+const Color kSecondryColor = Color(0xff);
+const Color kPrimaryTextColor = Color(0xff);
+const Color kSecondryTextColor = Color(0xff);
+
+const Duration kAnimationDuration = Duration(microseconds: 250);
---
+++ lib/theme/theme.dart
@@ -0,0 +1,8 @@
+import 'package:closer/theme/constants.dart';
+import 'package:flutter/material.dart';
+
+ThemeData appTheme() => ThemeData(
+      fontFamily: 'Poppins',
+      primaryColor: kPrimaryColor,
+      scaffoldBackgroundColor: Colors.white,
+    );
